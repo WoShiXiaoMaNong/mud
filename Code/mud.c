48d360b50356e99071ad94703dd8425a948e976a
Mrkkk <zhongming139@126.com>
2020-04-05 00:51:54 +0800

    Modify code from windows style to linux

--- Code/mud.c
+++ Code/mud.c
@@ -1,17 +1,14 @@
 
-#include <windows.h>
 #include "logger.h"
 #include "map.h"
 
 
 int main(void)
 {
-	char * filePath = "C:\\Users\\Administrator\\Desktop\\Mud\\design\\testMap.txt";
+	char * filePath = "../design/testMap.txt";
 	struct world* mud = (struct world*)malloc(sizeof(struct world*));
 	//while(1){
 	initWorld(mud,filePath);
 	//}
-	system("pause"); 
-	return 1;
-	
+	return 0;
 }
